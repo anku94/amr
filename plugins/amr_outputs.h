f249b63a4bf9530f4855f41c1197f755f8881c93
Ankush Jain <anku.94@gmail.com>
2022-05-23 21:51:43 -0400

    amr outputs: misc code

--- plugins/amr_outputs.h
+++ plugins/amr_outputs.h
@@ -1,9 +1,9 @@
 #pragma once
 
-class CSVFile {
+class MsgLog {
  public:
-  CSVFile(const char* dir, const char* fname, int rank)
-      : rank_(rank), file_(nullptr) {
+  MsgLog(const char* dir, int rank) : rank_(rank), file_(nullptr) {
+    const char* fname = "msgs";
     char fpath[4096];
     snprintf(fpath, 4096, "%s/%s.%d.csv", dir, fname, rank);
     file_ = fopen(fpath, "w+");
@@ -13,28 +13,8 @@ class CSVFile {
     WriteHeader();
   }
 
-  ~CSVFile() {
-    fclose(file_);
-    file_ = nullptr;
-  }
-
- protected:
-  virtual void WriteHeader();
-
-  int rank_;
-  FILE* file_;
-
-  std::mutex mutex_;
-  static const bool paranoid_ = false;
-};
-
-class MsgLog : public CSVFile {
- public:
-  MsgLog(const char* dir, int rank) : CSVFile(dir, "msgs", rank) {}
-
-  void LogMsg(int peer, int timestep, const char* phase,
-              uint64_t msg_id, int send_or_recv, uint64_t msg_sz,
-              uint64_t timestamp) {
+  void LogMsg(int peer, int timestep, const char* phase, uint64_t msg_id,
+              int send_or_recv, uint64_t msg_sz, uint64_t timestamp) {
     if (paranoid_) mutex_.lock();
 
     const char* fmt = "%d,%d,%d,%s,%" PRIu64 ",%d,%" PRIu64 ",%" PRIu64 "\n";
@@ -45,25 +25,50 @@ class MsgLog : public CSVFile {
   }
 
  private:
-  void WriteHeader() override {
+  void WriteHeader() {
     const char* const header =
         "rank,peer,timestep,phase,msg_id,send_or_recv,msg_sz,timestamp\n";
     fprintf(file_, header);
   }
+
+  int rank_;
+  FILE* file_;
+
+  std::mutex mutex_;
+  static const bool paranoid_ = false;
 };
 
-class FuncLog : public CSVFile {
+class FuncLog {
  public:
-  FuncLog(const char* dir, int rank) : CSVFile(dir, "funcs", rank) {}
+  FuncLog(const char* dir, int rank) : rank_(rank), file_(nullptr) {
+    const char* fname = "funcs";
+    char fpath[4096];
+    snprintf(fpath, 4096, "%s/%s.%d.csv", dir, fname, rank);
+    file_ = fopen(fpath, "w+");
+    if (file_ == nullptr) {
+      ABORT("Failed to open CSV");
+    }
+    WriteHeader();
+  }
 
   void LogFunc(const char* func_name, uint64_t timestamp, bool enter_or_exit) {
+    if (strncmp(func_name, "Task_ReceiveBoundaryBuffers_MeshBlockData", 41) ==
+        0)
+      return;
+
     const char* fmt = "%d,%" PRIu64 ",%s,%c\n";
     fprintf(file_, fmt, rank_, timestamp, func_name, enter_or_exit ? '0' : '1');
   }
 
  private:
-  void WriteHeader() override {
+  void WriteHeader() {
     const char* const header = "rank,timestep,func,enter_or_exit";
     fprintf(file_, header);
   }
+
+  int rank_;
+  FILE* file_;
+
+  std::mutex mutex_;
+  static const bool paranoid_ = false;
 };
