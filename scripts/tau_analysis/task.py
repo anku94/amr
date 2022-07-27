3114cfecd53a55dba7974fdb464c249d9a1315d9
Ankush Jain <anku.94@gmail.com>
2022-07-27 00:49:26 -0400

    Task: support bad nodes/overloaded nodes etc

--- scripts/tau_analysis/task.py
+++ scripts/tau_analysis/task.py
@@ -7,6 +7,20 @@ import time
 import traceback
 from typing import Tuple
 
+
+def get_node_idx() -> Tuple[int, int]:
+    result = subprocess.run(
+        ["/share/testbed/bin/emulab-listall"], stdout=subprocess.PIPE
+    )
+    hoststr = str(result.stdout.decode("ascii"))
+    hoststr = hoststr.strip().split(",")
+    num_hosts = len(hoststr)
+    our_id = open("/var/emulab/boot/nickname", "r").read().split(".")[0][1:]
+    our_id = int(our_id)
+
+    return (our_id, num_hosts)
+
+
 """
 Task module exposes two levels of parallelism:
 1. Intra-node: One task is called per-node. It will spawn nworkers threads
@@ -18,33 +32,53 @@ Task module exposes two levels of parallelism:
 
 class Task:
     nranks = 512
-    nperrank = 16
+    npernode = 16
     nworkers = 16
 
     def __init__(self, trace_dir):
         self._trace_dir = trace_dir
+        self._bad_nodes = set()
+        self._halfwork_nodes = set()
 
-        hidx, nhosts = self.get_node_idx()
+        hidx, nhosts = get_node_idx()
         self.hidx = hidx
         self.nhosts = nhosts
 
-    @classmethod
-    def get_node_idx(cls) -> Tuple[int, int]:
-        result = subprocess.run(
-            ["/share/testbed/bin/emulab-listall"], stdout=subprocess.PIPE
-        )
-        hoststr = str(result.stdout.decode("ascii"))
-        hoststr = hoststr.strip().split(",")
-        num_hosts = len(hoststr)
-        our_id = open("/var/emulab/boot/nickname", "r").read().split(".")[0][1:]
-        our_id = int(our_id)
+    def gen_worker_fn_args(self):
+        return {}
 
-        return (our_id, num_hosts)
+    def _gen_work_map(self):
+        nidx = 0
+        rbeg = 0
+        rend = 0
 
-    def gen_worker_fn_args(self, rank):
-        fn_args = {"rank": rank}
+        alloc_map = {}
 
-        return fn_args
+        while rbeg < self.nranks:
+            if nidx in self._bad_nodes:
+                nidx += 1
+                continue
+
+            work_units = self.npernode
+            if nidx in self._halfwork_nodes:
+                work_units = int(work_units / 2)
+
+            rend = rbeg + work_units
+            cur_alloc = rbeg, rend
+
+            alloc_map[nidx] = cur_alloc
+
+            rbeg = rend
+            nidx += 1
+
+        return alloc_map
+
+    def skip_node(self, nidx):
+        self._bad_nodes.add(nidx)
+        pass
+
+    def half_node_alloc(self, nidx):
+        self._halfwork_nodes.add(nidx)
 
     @staticmethod
     def worker(fn_args):
@@ -52,11 +86,27 @@ class Task:
         print("Worker {}: {}".format(rank, fn_args))
         time.sleep(1)
 
-    def run_worker(self):
-        rbeg = self.nperrank * self.hidx
-        rend = rbeg + self.nperrank
+    def run_node(self, nidx=-1):
+        if nidx == -1:
+            nidx, _ = get_node_idx()
 
-        rend = min(rend, self.nranks)
+        node_work_map = self._gen_work_map()
+
+        if nidx not in node_work_map:
+            print('Node {} not allocated any work!'.format(nidx))
+            return
+
+        rbeg, rend = node_work_map[nidx]
+        return self.run_rankwise(rbeg, rend)
+
+    def run_rankwise(self, rbeg=-1, rend=-1):
+        if rbeg < 0:
+            rbeg = self.npernode * self.hidx
+
+        if rend < 0:
+            rend = rbeg + self.npernode
+            rend = min(rend, self.nranks)
+
         self.log("Running workers from {} to {}".format(rbeg, rend))
 
         base_fn_args = self.gen_worker_fn_args()
@@ -64,24 +114,28 @@ class Task:
         for rank in range(rbeg, rend):
             fn_args = copy.deepcopy(base_fn_args)
             fn_args["rank"] = rank
+            fn_args["trace_dir"] = self._trace_dir
             all_fn_args.append(fn_args)
 
+        return self.run_pool(all_fn_args)
+
+    def run_pool(self, all_fn_args):
         self.log("Starting pool with {} workers".format(self.nworkers))
 
-        #  with multiprocessing.Pool(processes=self.nworkers) as p:
-        #  p.map(self.worker, all_fn_args)
+        all_data = []
+
         with ProcessPoolExecutor(max_workers=self.nworkers) as e:
-            #  ret = e.map(self.worker, all_fn_args)
-            #  print(ret)
             futures = {e.submit(self.worker, fn_arg): fn_arg for fn_arg in all_fn_args}
             for future in as_completed(futures):
                 try:
                     data = future.result()
-                    print(data)
+                    all_data.append(data)
                 except Exception as e:
                     print(futures[future])
                     print(e)
                     traceback.print_exc()
+
+            return all_data
 
     def log(self, logstr):
         print("h{}: {}".format(self.hidx, logstr))
@@ -90,4 +144,13 @@ class Task:
 if __name__ == "__main__":
     trace_dir = "/mnt/ltio/parthenon-topo/profile8"
     t = Task(trace_dir)
-    t.run_worker()
+    t.skip_node(9)
+    t.half_node_alloc(10)
+    t.half_node_alloc(12)
+    m = t._gen_work_map()
+    print(m)
+    t.run_node(9)
+    #  t.run_node(10)
+    #  t.run_node(11)
+    #  t.run_node(12)
+    # t.run_rankwise()
