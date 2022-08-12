0187cf61d1c8bb5c5692038c931bc1583f2459a9
Ankush Jain <ankushj@andrew.cmu.edu>
2022-08-12 15:47:00 -0400

    topobench: add testing skeleton whooo

---
+++ tools/topobench/tests/topogen_test.cc
@@ -0,0 +1,14 @@
+//
+// Created by Ankush J on 8/12/22.
+//
+
+#include <gtest/gtest.h>
+#include <topology.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
