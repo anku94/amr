692058338e7d8773d905dcb5bf0a02ff887c1170
Ankush Jain <anku.94@gmail.com>
2025-04-17 12:11:38 -0400

    Cleanup logging

--- tools/topobench/src/meshdriver_main.cc
+++ tools/topobench/src/meshdriver_main.cc
@@ -3,17 +3,22 @@
 
 #include "bench/mesh_driver.h"
 #include "amr/mesh_utils.h"
+#include "topo_common.h"
 
 using namespace topo::amr;
 
 int main(int argc, char *argv[]) {
+    FLAGS_logtostderr = true;
+    FLAGS_log_prefix = false;
+    google::InitGoogleLogging(argv[0]);
+
     MPI_Init(&argc, &argv);
 
     int my_rank, nranks;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
 
-    printf("Hello, world! I am rank %d of %d\n", my_rank, nranks);
+    MLOG(LOG_INFO, "Hello, world! I am rank %d of %d", my_rank, nranks);
 
     topo::bench::MeshDriverOpts opts;
     opts.mesh_dims = Vec3i(2, 2, 2);
--- tools/topobench/src/topo_common.h
+++ tools/topobench/src/topo_common.h
@@ -1,6 +1,41 @@
 #pragma once
 
-#include <glog/logging.h> // IWYU pragma: export
+#include <glog/logging.h>  // IWYU pragma: export
 
-#define MLOG(lvl, fmt, ...) \
-  logv(__LOG_ARGS__, lvl, fmt, ##__VA_ARGS__)
+inline std::string fmtstr(const char *fmt, ...) {
+  char buf[1024];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buf, sizeof(buf), fmt, args);
+  va_end(args);
+  return std::string(buf);
+}
+
+#define MLOG_INNER(lvl, fmt, ...)                             \
+  do {                                                        \
+    switch (lvl) {                                            \
+      case LOG_ERRO:                                          \
+        LOG(ERROR) << fmtstr("[ERRO] " fmt, ##__VA_ARGS__);   \
+        break;                                                \
+      case LOG_WARN:                                          \
+        LOG(WARNING) << fmtstr("[WARN] " fmt, ##__VA_ARGS__); \
+        break;                                                \
+      case LOG_INFO:                                          \
+        LOG(INFO) << fmtstr("[INFO] " fmt, ##__VA_ARGS__);    \
+        break;                                                \
+      case LOG_DBUG:                                          \
+        VLOG(0) << fmtstr("[DBG0] " fmt, ##__VA_ARGS__);      \
+        break;                                                \
+      case LOG_DBG2:                                          \
+        VLOG(2) << fmtstr("[DBG2] " fmt, ##__VA_ARGS__);      \
+        break;                                                \
+      case LOG_DBG3:                                          \
+        VLOG(3) << fmtstr("[DBG3] " fmt, ##__VA_ARGS__);      \
+        break;                                                \
+    }                                                         \
+  } while (0)
+
+#define MLOG(level, fmt, ...)                                                  \
+  MLOG_INNER(level, "[%10.10s:%3.3d] " fmt,                                    \
+             (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__), \
+             __LINE__, ##__VA_ARGS__)
