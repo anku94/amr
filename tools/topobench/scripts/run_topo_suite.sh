3045a0a8e82cea9833c909354a31b15523919d39
Ankush Jain <anku.94@gmail.com>
2025-04-18 19:31:57 -0400

    misc

--- tools/topobench/scripts/run_topo_suite.sh
+++ tools/topobench/scripts/run_topo_suite.sh
@@ -5,28 +5,45 @@ set -eu
 dims_xyz=2
 max_reflvl=4
 target_leafcnt=16
+
 all_policies=( "baseline" "cdp" "hybrid25" "hybrid50" "hybrid75" "lpt" )
-#all_policies=( "hybrid25" )
+all_policies=( "hybrid25" )
+
 jobdir=/tmp
+
 all_num_ts=( 1 10 100 )
 all_num_ts=( 1 )
+
 all_num_rounds=( 1 10 100 )
 all_num_rounds=( 1 )
 
+# AMRLB_INSTALLDIR=??
+if [ -z "${AMRLB_INSTALLDIR:-}" ]; then
+    echo "AMRLB_INSTALLDIR is not set"
+    echo "Please set AMRLB_INSTALLDIR to the installation directory of AMR-LB"
+    exit 1
+fi
+
 # TODO: set AMRLB_CONFIG and AMRLB_LOGDIR
-TOPOBENCH_BIN=/Users/schwifty/Repos/amr/build/tools/topobench/src/meshdriver_main
+TOPOBENCH_BIN=$AMRLB_INSTALLDIR/bin/meshdriver_main
+[ ! -f $TOPOBENCH_BIN ] && {
+    echo "TOPOBENCH_BIN $TOPOBENCH_BIN does not exist"
+    exit 1
+}
 
 # getdatetime: returns YYYYMMDD_HHMMSS
 getdatetime() {
     date +%Y%m%d_%H%M%S
 }
 
+# getlogfname: returns log file name
 getlogfname() {
     # mrl: max_revlvl
     # tlc: target_leafcnt
     echo "topobench_mesh_$(getdatetime)_dims${dims_xyz}_mrl${max_reflvl}_tlc${target_leafcnt}.csv"
 }
 
+# message: prints a message
 message() {
     echo "[INFO] $@"
 }
@@ -58,34 +75,42 @@ gen_config() {
         $cfg_template > $cfg_out
 }
 
+# gen_cfg_test: generates a config file for testing
 gen_cfg_test() {
     cfg_template=config/template.cfg
     cfg_out=config/cfg-test.cfg
     gen_config $cfg_template $cfg_out
 }
 
+# run_single: runs a single combination of policy, num_ts, num_rounds
 run_single() {
     message "Running with policy $policy"
 
-    cfg_template=config/template.cfg
-    cfg_out=config/cfg-test.cfg
+    # gen a config
+    cfg_template=$AMRLB_INSTALLDIR/config/template.cfg
+    cfg_out=$AMRLB_INSTALLDIR/config/cfg-test.cfg
     gen_config $cfg_template $cfg_out
 
+    # set env vars for exec
     export AMRLB_CONFIG=$cfg_out
     export AMRLB_LOGDIR=$jobdir
-    GLOG_v=-1 mpirun -np 8 $TOPOBENCH_BIN
+
+    # run the thing
+    # GLOG_v=-1 mpirun -np 8 $TOPOBENCH_BIN
+    GLOG_v=-1 $TOPOBENCH_BIN
 }
 
+# run: runs the suite
 run() {
     log_fname=$(getlogfname)
-
     message "Log file: $log_fname"
     # gen_cfg_test
+
     for policy in ${all_policies[@]}; do
         for num_ts in ${all_num_ts[@]}; do
             for num_rounds in ${all_num_rounds[@]}; do
                 message "Running with policy $policy, num_ts $num_ts, num_rounds $num_rounds"
-                # run_single
+                run_single
             done
         done
     done
